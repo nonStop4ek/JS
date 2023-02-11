4e84437342703be4f77e29ed52acb0e0672cb617
nonStop4ek <Nikita1stgod@gmail.com>
2023-02-11 18:39:14 +0300

    full HW with default

---
+++ lessson4/function1.js
@@ -0,0 +1,33 @@
+
+// - створити функцію яка обчислює та повертає площу прямокутника зі сторонами а і б
+
+// {
+//     function area(a,b) {
+//
+//
+//         document.write(`<div> ${a * b}</div>`)
+//     }
+//
+//     area(4,6);
+//     area(2,8);
+//     area(3,33);
+// }
+
+// - створити функцію яка обчислює та повертає площу кола з радіусом r
+
+
+// {
+//     function arearkolo(radius) {
+//         let result = Math.PI * Math.pow(radius, 2);
+//         document.write(`<div>    ${result}</div>`);
+//         return result;
+//         return radius;
+//     }
+//
+//     arearkolo(5)
+//     arearkolo(2)
+// }
+
+
+// - створити функцію яка обчислює та повертає площу циліндру висотою h, та радіутом r
+
