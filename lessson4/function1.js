3489334ae9358676a7bb7598dad591921fb921da
nonStop4ek <Nikita1stgod@gmail.com>
2023-02-12 15:46:38 +0300

    full HW with default

--- lessson4/function1.js
+++ lessson4/function1.js
@@ -1,4 +1,3 @@
-
 // - створити функцію яка обчислює та повертає площу прямокутника зі сторонами а і б
 
 // {
@@ -43,3 +42,115 @@
 //     cylinder(3, 4)
 // }
 
+// - створити функцію яка приймає масив та виводить кожен його елемент
+
+// {
+//     function arr(...x) {
+//         console.log(x)
+//
+//     }
+//
+//     arr(1, 2, 3, 4, 5, 6, 7, 8)
+// }
+
+// - створити функцію яка створює параграф з текстом. Текст задати через аргумент
+
+// {
+//     function paragraph(...x) {
+//         document.write(`<div><p>${x}</p></div>`)
+//
+//     }
+//
+//     paragraph('la la la')
+//     paragraph('ad','ad','ad','da')
+// }
+
+// - створити функцію яка створює ul з трьома елементами li.
+// Текст li задати через аргумент всім однаковий
+
+// {
+//     function ulLI(...x) {
+//         document.write(`<div>
+// <ul>
+// <li>${x}</li><li>${x}</li><li>${x}</li>
+// </ul>
+//
+// </div>`)
+//
+//     }
+//
+//     ulLI('li', 'boo', 'foo')
+//     ulLI('lid', 'bood', 'food')
+//
+// }
+
+
+// - створити функцію яка створює ul з трьома елементами li.
+// Текст li задати через аргумент всім однаковий. Кількість
+// li визначається другим аргументом, який є числовим (тут використовувати цикл)
+
+// {
+//     function ulli(...x) {
+//         for (let i = 0; i < x.length; i++) {
+//             // const yElement = x[i];
+//
+//
+//             document.write(`
+//
+//     <div>
+//             <ul>
+//                         <li>${x[i]}</li>
+//             </ul>
+//     </div>
+//     `)
+//         }
+//     }
+//
+//     ulli('wa', 'as', 'awd')
+// }
+
+// - створити функцію яка приймає масив примітивних елементів (числа,стрінги,булеві),
+// та будує для них список
+
+// {
+//     function NSB(...x) {
+//         for (let i = 0; i < x.length; i++) {
+//             // const x1 = x[i];
+//
+//
+//             document.write(`<div>
+//                             <ul>
+//                                     <li>${x[i]}</li>
+//                             </ul>
+//                     </div>`)
+//         }
+//     }
+//
+//     NSB(2, 'asda', Boolean)
+// }
+
+
+// створити функцію яка приймає масив об'єктів з наступними полями id,name,age ,
+// та виводить їх в документ. Для кожного об'єкту окремий блок.
+
+function arrayOfObjects(array) {
+    for (let arrayElement of array) {
+        document.write('<div>')
+        for (let arrayElementKey in arrayElement) {
+            document.write(`<div>${arrayElementKey} ${arrayElement[arrayElementKey]}</div>`)
+        }
+        document.write('</div> <br>')
+    }
+
+
+}
+
+arrayOfObjects([{id: 12, name: 'Nikita', age: 32}, {id: 23, name: 'bads', age: 12}, {
+    id: 12,
+    name: 'Nikita',
+    age: 32
+}, {id: 23, name: 'bads', age: 12}])
+
+
+// - створити функцію яка повертає найменьше число з масиву
+// - створити функцію sum(arr)яка приймає масив чисел, сумує значення елементів масиву та повертає його. Приклад sum([1,2,10]) //->13
