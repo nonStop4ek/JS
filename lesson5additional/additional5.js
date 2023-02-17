0b9f25efab1792ca46fdf1ab00c532889e5961d7
nonStop4ek <Nikita1stgod@gmail.com>
2023-02-17 16:57:51 +0300

    full HW with default

--- lesson5additional/additional5.js
+++ lesson5additional/additional5.js
@@ -1,35 +1,43 @@
-// створити функцію яка приймає три числа та виводить найменьше. (Без Math.min!)
-{
-    let qwe = (array) => {
-        let a = array[0]
-        for (let i = 0; i < array.length; i++) {
-            // const arrayElement = array[i];
-            if (array[0] > array[i]) {
-                a = array[i]
-                console.log(a);
-            } else if (array[i] < array[0]) {
-                console.log(a)
-            }
-        }
+// створити функцію яка приймає три числа та виводить найменьше.
 
-    }
-    qwe([1, 2, 3, -5])
-}
 
-// створити функцію яка приймає три числа та виводить найбільше. (Без Math.max!)
-{
-    let qwe = (array) => {
-        let a = array[0]
-        for (let i = 0; i < array.length; i++) {
 
-            if (array[0] < array[i]) {
-                a = array[i]
 
-            } else if (array[i] < array[0]) {
 
-            }
-        }
-        console.log(a);
-    }
-    qwe([1, 2, 3, -5,9])
-}
+
+// - створити функцію яка повертає найменьше число з масиву
+
+// {
+//     let qwe = (array) => {
+//         let a = array[0]
+//         for (let i = 0; i < array.length; i++) {
+//             // const arrayElement = array[i];
+//             if (array[0] > array[i]) {
+//                 a = array[i]
+//                 console.log(a);
+//             } else if (array[i] < array[0]) {
+//                 console.log(a)
+//             }
+//         }
+//
+//     }
+//     qwe(1, 2, 3, -5)
+// }
+
+// створити функцію яка повертає найбільше число з масиву
+// {
+//     let qwe = (array) => {
+//         let a = array[0]
+//         for (let i = 0; i < array.length; i++) {
+//
+//             if (array[0] < array[i]) {
+//                 a = array[i]
+//
+//             } else if (array[i] < array[0]) {
+//
+//             }
+//         }
+//         console.log(a);
+//     }
+//     qwe([1, 2, 3, -5,9])
+// }
